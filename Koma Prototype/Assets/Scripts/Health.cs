ee28677a114cdd66653b03791a7b3892ed0dcbd8
Videt <videt@users.noreply.github.com>
2020-06-19 23:17:57 +1100

    Health system

---
+++ Koma Prototype/Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] int numberOfHearts;
+
+    [SerializeField] Image[] hearts;
+    [SerializeField] Sprite fullHeart;
+    [SerializeField] Sprite emptyHeart;
+
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (health > numberOfHearts)
+            {
+                health = numberOfHearts;
+            }
+
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i<numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
