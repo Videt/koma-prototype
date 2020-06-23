a891facd3ab9af320f9f15db7a8d78f670d10a17
Videt <videt@users.noreply.github.com>
2020-06-23 23:58:27 +1100

    Respawn when the hero dies

--- Koma Prototype/Assets/Scripts/Health.cs
+++ Koma Prototype/Assets/Scripts/Health.cs
@@ -12,13 +12,26 @@ public class Health : MonoBehaviour
     [SerializeField] Sprite fullHeart;
     [SerializeField] Sprite emptyHeart;
 
+    Rigidbody2D hero;
+    float posX, posY; // изначальные позиции
     public void TakeEnemyDamage(int attackdamage)
     {
         health -= attackdamage;
-
+    }
+    void Start()
+    {
+        hero = GetComponent<Rigidbody2D>();
+        posX = hero.position.x;
+        posY = hero.position.y;
     }
     void Update()
     {
+        if (health <= 0)
+        {
+            hero.position = new Vector2(posX, posY);
+            health = 4;
+        }
+
         for (int i = 0; i < hearts.Length; i++)
         {
             if (health > numberOfHearts)
@@ -35,7 +48,7 @@ public class Health : MonoBehaviour
                 hearts[i].sprite = emptyHeart;
             }
 
-            if (i<numberOfHearts)
+            if (i < numberOfHearts)
             {
                 hearts[i].enabled = true;
             }
