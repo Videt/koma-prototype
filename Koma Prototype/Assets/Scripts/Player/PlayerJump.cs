b4b4f9e033f59369dc3d4ba9a889481ac379ff24
Videt <videt@users.noreply.github.com>
2020-07-16 22:57:45 +1100

    Частично переделал управление
    В старом PlayerMovement ничего не менял, только отключил его на персонаже. Добавил два новых скрипта со старой системой управления PlayerMove и PlayerJump. Еще не все управление перенес, надо будет потом дополнить

---
+++ Koma Prototype/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D hero;
+
+    public float jumpForce = 1000f;
+    public bool isJumping;
+    public bool isGrounded = false;
+    float groundRadius = 0.1f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    public Transform Effect;
+    //public Animator anim;
+    void Start()
+    {
+        hero = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        Jump();
+    }
+
+    void Jump()
+    {
+        isJumping = Input.GetButton("Jump");
+        if (isGrounded && isJumping == true)
+        {
+            hero.AddForce(new Vector2(0, jumpForce));
+            //anim.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Koma Prototype/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D hero;
+
+    //ходьба и бег
+    public float speed = 0f;
+    public float walkSpeed = 10f;
+    public float runSpeed = 100f;
+    public float horizontalMove;
+
+    //анимация ходьбы и бега
+    /*public Animator anim;
+    public bool isRunning = false;
+    public bool isWalking = false;*/
+
+    void Start()
+    {
+        hero = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+        if (horizontalMove < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (horizontalMove > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = runSpeed;
+        else
+            speed = walkSpeed;
+
+        horizontalMove = Input.GetAxis("Horizontal");
+        hero.velocity = new Vector2(horizontalMove * speed, hero.velocity.y);
+
+        /*if (speed == walkSpeed && horizontalMove != 0)
+             isWalking = true;
+         else if (speed == runSpeed)
+             isRunning = true;
+
+         if (isWalking == true && horizontalMove != 0)
+         {
+             anim.SetBool("isWalking", true);
+         }
+         else
+         {
+             anim.SetBool("isWalking", false);
+         }
+         if (isRunning == true)
+         {
+             anim.SetBool("isRunning", true);
+         }
+         else
+         {
+             anim.SetBool("isRunning", false);
+         }*/
+    }
+}
