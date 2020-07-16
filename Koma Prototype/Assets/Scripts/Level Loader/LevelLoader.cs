42adde30bf3f9461dccfbfccf79025c4c74c2176
Videt <videt@users.noreply.github.com>
2020-07-16 19:20:22 +1100

    Сделал переходы между сценами
    Чтобы аквтивировать переход в новой сцене - просто добавьте Level Loader из папки Prefabs в иерархию. Пока что сделал смену сцен на кнопку "L"

---
+++ Koma Prototype/Assets/Scripts/Level Loader/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transtitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextScene();
+        }
+    }
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transtitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
