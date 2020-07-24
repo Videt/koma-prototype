4a73a143d04e8a446d48b4abb4c71c67776f64cd
Videt <videt@users.noreply.github.com>
2020-07-24 21:15:18 +1100

    Revert "Revert "Merge branch 'master' of https://github.com/Videt/koma-prototype""
    This reverts commit 66253e1aa22aebe9fea60fdddddfee17d25270c9.

---
+++ Koma Prototype/Assets/FlyingEnemyGraphics.cs
@@ -0,0 +1,16 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyGraphics : MonoBehaviour
+{
+    public AIPath aiPath;
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+            transform.localScale = new Vector3(-1f, 1f, 1f); //если движется вправо, то поворот
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+            transform.localScale = new Vector3(1f, 1f, 1f); //если движется влево, то поворот
+    }
+}
