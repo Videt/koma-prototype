0088c275377bbab540d41b63c75d5ffabfe147e4
Videt <videt@users.noreply.github.com>
2020-07-24 23:03:32 +1100

    Летающий враг
    У летучей мыши теперь есть спрайт.
    Добавил префаб в папку prefabs;
    чтобы она следовала за игроком нужно в инспекторе во вкладке ai destination setter в строчку target перенести персонажа

--- Koma Prototype/Assets/Scripts/Flying Enemy/FlyingEnemyGraphics.cs
+++ Koma Prototype/Assets/Scripts/Flying Enemy/FlyingEnemyGraphics.cs
@@ -9,8 +9,14 @@ public class FlyingEnemyGraphics : MonoBehaviour
     void Update()
     {
         if (aiPath.desiredVelocity.x >= 0.01f)
-            transform.localScale = new Vector3(-1f, 1f, 1f); //если движется вправо, то поворот
+        {
+            transform.localScale = new Vector3(1f, 1f, -1f); //если движется вправо, то поворот
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
         else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
             transform.localScale = new Vector3(1f, 1f, 1f); //если движется влево, то поворот
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
