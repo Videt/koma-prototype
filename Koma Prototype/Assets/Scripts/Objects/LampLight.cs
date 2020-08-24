7e921ff9e080289f718cb61707f1055f46255f82
DE-LOG <58259565+DE-LOG@users.noreply.github.com>
2020-08-24 12:25:23 +0500

    добавил свет, возможность его включать и выкл. + брать в руки предметы

---
+++ Koma Prototype/Assets/Scripts/Objects/LampLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampLight : MonoBehaviour
+{
+    public GameObject Light;
+    public bool On;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
