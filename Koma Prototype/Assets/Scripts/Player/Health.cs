40e3c81249750eaec9e7607aeb1d8c0c36ebbc38
Videt <videt@users.noreply.github.com>
2020-09-07 00:04:18 +1100

    Вернул систему здоровья назад

--- Koma Prototype/Assets/Scripts/Player/Health.cs
+++ Koma Prototype/Assets/Scripts/Player/Health.cs
@@ -6,27 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] int health;
-    [SerializeField] int numberOfHearts;
+    public int health = 5;
+    public int numberOfHearts = 10;
 
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
-    GameMaster gm;
     public void TakeEnemyDamage(int attackdamage)
     {
         health -= attackdamage;
     }
     void Start()
     {
-        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
-        transform.position = gm.lastCheckpointPos;
+        
     }
     void Update()
     {
+        if (health > numberOfHearts)
+            health = numberOfHearts;
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+                hearts[i].sprite = fullHeart;
+            else
+                hearts[i].sprite = emptyHeart;
+
+            if (i < numberOfHearts)
+                hearts[i].enabled = true;
+            else
+                hearts[i].enabled = false;
+        }
         if (health <= 0)
         {
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene);
-            health = 4;
+            health = 5;
         }      
     }
 }
