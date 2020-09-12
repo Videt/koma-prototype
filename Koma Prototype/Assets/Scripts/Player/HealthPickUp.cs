7fccfc3809b7bd359b8a927db338fa0a0fc2a6eb
Videt <videt@users.noreply.github.com>
2020-09-12 21:17:56 +1100

    Лечение
    Стандартная система: подбираешь сердечки и лечишься. Нужно на каждое сердечко цеплять бокс коллайдер и ставить is trigger

---
+++ Koma Prototype/Assets/Scripts/Player/HealthPickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour
+{
+    Health health;
+    public int bonusHealth = 1;
+    void Awake()
+    {
+        health = FindObjectOfType<Health>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (health.health < health.maxHealth)
+        {
+            Destroy(gameObject);
+            health.health += bonusHealth;
+        }
+    }
+}
