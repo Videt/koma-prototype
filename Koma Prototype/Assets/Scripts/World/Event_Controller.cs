89d3e2a5da4a71611c7c64983d30ebe8b1fc9be5
DE-LOG <58259565+DE-LOG@users.noreply.github.com>
2020-09-19 16:17:47 +0500

    19.09.2020

---
+++ Koma Prototype/Assets/Scripts/World/Event_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Event_Controller : MonoBehaviour
+{   public UnityEvent MyEvent;
+
+    
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            MyEvent.Invoke();
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
