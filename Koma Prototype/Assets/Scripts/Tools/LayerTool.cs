b75d65e10b01f8a05c9628d1f6e0dc50be2a9390
Videt <videt@users.noreply.github.com>
2020-09-20 00:33:57 +1100

    Изменения порядка слоев в зависимости от Z
    Order in layer меняется в зависимости от координаты по Z. Нужно повесить скрипт LayerTool на каждый объект со спрайтом

---
+++ Koma Prototype/Assets/Scripts/Tools/LayerTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class LayerTool : MonoBehaviour
+{
+    public SpriteRenderer sp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        sp.sortingOrder = Mathf.FloorToInt(transform.position.z);
+    }
+    private void OnEnable()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+}
