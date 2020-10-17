d77b8db66494aeba1f2dde4581f616b1bd1441a9
lowpolyplanet <slimetotime@gmail.com>
2020-10-17 12:39:23 +0300

    Lamp + bugFix
    Исправил баги с эвент контроллером, который ругался на всё подряд и добавил лампу.

--- Koma Prototype/Assets/Scripts/Objects/LampLight.cs
+++ Koma Prototype/Assets/Scripts/Objects/LampLight.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class LampLight : MonoBehaviour
 {
-    public GameObject Light;
-    public bool On;
+    private Light2D lightLamp;
+    public bool isLighting = true;
+    public float smooth_swith;
+    [Range(0,5)]
+    public float intensity_light;
+
+
+
     void Start()
     {
-        
+        lightLamp = gameObject.GetComponent<Light2D>();
+        intensity_light = lightLamp.intensity;
+    }
+    private void Update()
+    {
+        if (isLighting)
+        {       
+            if (lightLamp.intensity < intensity_light)
+            {
+                lightLamp.intensity += smooth_swith;
+            }
+        }
+        else
+        {
+            if (lightLamp.intensity > 0)
+            {
+                lightLamp.intensity -= smooth_swith;
+            }
+        }
+    }
+    public void Swith()
+    {
+        isLighting = !isLighting;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
