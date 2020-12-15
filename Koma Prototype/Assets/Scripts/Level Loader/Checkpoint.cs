d02989207ccad9c3ef2d10f4ae5108d0bc34df17
Videt <egor310501@mail.ru>
2020-12-15 20:09:12 +1100

    вернул примитивные чекпоинты
    добавьте в сцену префабы game master и checkpoint и повесьте на игрока скрипт playerPos

---
+++ Koma Prototype/Assets/Scripts/Level Loader/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    GameMaster gm;
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gm.lastCheckpointPos = transform.position;
+        }
+    }
+}
